992e296f052f1160f764ff8cd555526312ed7c0e
Omkar Ingale <email.omkaringale@gmail.com>
2022-01-20 13:36:33 +0530

    Used array of balls

--- src/main/java/OopMethod.java
+++ src/main/java/OopMethod.java
@@ -3,10 +3,7 @@ import processing.core.PApplet;
 public class OopMethod extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
-    Balls ball1;
-    Balls ball2;
-    Balls ball3;
-    Balls ball4;
+    public static Balls[] ball= new Balls[4];
 
     public static void main(String[] args) {
         PApplet.main("OopMethod");
@@ -18,20 +15,15 @@ public class OopMethod extends PApplet {
 
     public void setup(){
         int generalHeight= HEIGHT/5;
-        ball1= new Balls(1*generalHeight, 1, this);
-        ball2= new Balls(2*generalHeight, 2, this);
-        ball3= new Balls(3*generalHeight, 3, this);
-        ball4= new Balls(4*generalHeight, 4, this);
+        for(int i=1; i<=4; i++){
+            ball[i-1]= new Balls(i*generalHeight, i, this);
+        }
     }
 
     public void draw(){
-        ball1.Display();
-        ball1.move();
-        ball2.Display();
-        ball2.move();
-        ball3.Display();
-        ball3.move();
-        ball4.Display();
-        ball4.move();
+        for(int i=0; i<4; i++){
+            ball[i].Display();
+            ball[i].move();
+        }
     }
 }
